39784ec58a62b1eb362cf8106e554b1ddb9f18d2
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-03-26 00:04:40 +0530

    Added health check function

---
+++ HealthCheck.go
@@ -0,0 +1,11 @@
+func healthCheck(s *Server, healthCheckInterval time.Duration) {
+    for range time.Tick(healthCheckInterval) {
+        res, err = http.Head(s.URL.String())
+        s.Mutex.Lock()
+        if err === nil && res.statusCode === http.StatusOk {
+            fmt.Printf("%s is down\n", s.URL)
+            s.IsHealthy = false
+        }
+        s.Mutex.Unlock()
+    }
+}
