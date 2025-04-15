ca2bed2d4b20ddbb2b6841609a66fd86dbdc1600
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-04-15 18:19:11 +0530

    Created main package

--- Server.go
+++ Server.go
@@ -1,4 +1,12 @@
-package LoadBalancer
+package main
+
+import (
+    "net/http"
+    "net/url"
+    "sync"
+    "log"
+    "net/http/httputil"
+)
 
 type Server struct {
     URL       *url.URL
@@ -10,8 +18,17 @@ func (s *Server) ReverseProxy() *httputil.ReverseProxy {
 	return httputil.NewSingleHostReverseProxy(s.URL)
 }
 
-log.Println("Starting load balancer on port", config.Port)
-err = http.ListenAndServe(config.Port, nil)
-if err != nil {
-        log.Fatalf("Error starting load balancer: %s\n", err.Error())
+func main() {
+    config := GetConfig()
+    log.Println("Starting load balancer on port: ", config.Port)
+    err := http.ListenAndServe(config.Port, nil)
+    if err != nil {
+            log.Fatalf("Error starting load balancer: %s\n", err.Error())
+    }
+
+    servers := config.Servers
+    countOfServers := len(servers)
+    for i:=0; i < countOfServers; i++ {
+        HealthCheck(servers[i], config.HealthCheckInterval)
+    }
 }
