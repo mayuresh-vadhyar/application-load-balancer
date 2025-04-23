74b2622a4bc0d6d073830ab2fa742cfbc72e6f5a
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-04-23 21:00:27 +0530

    Added function to initialize server list

--- Server.go
+++ Server.go
@@ -1,34 +1,54 @@
 package main
 
 import (
-    "net/http"
-    "net/url"
-    "sync"
-    "log"
-    "net/http/httputil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
 )
 
 type Server struct {
-    URL       *url.URL
-    IsHealthy bool
-    Mutex     sync.Mutex
+	URL       *url.URL
+	IsHealthy bool
+	Mutex     sync.Mutex
 }
 
 func (s *Server) ReverseProxy() *httputil.ReverseProxy {
 	return httputil.NewSingleHostReverseProxy(s.URL)
 }
 
-func main() {
-    config := GetConfig()
-    log.Println("Starting load balancer on port: ", config.Port)
-    err := http.ListenAndServe(config.Port, nil)
-    if err != nil {
-            log.Fatalf("Error starting load balancer: %s\n", err.Error())
-    }
+func CreateServerList(serverUrls []string) []*Server {
+	var servers []*Server
 
-    servers := config.Servers
-    countOfServers := len(servers)
-    for i:=0; i < countOfServers; i++ {
-        HealthCheck(servers[i], config.HealthCheckInterval)
-    }
+	for _, rawUrl := range serverUrls {
+		parsedUrl, err := url.Parse(rawUrl)
+		if err != nil {
+			continue
+		}
+
+		server := &Server{
+			URL:       parsedUrl,
+			IsHealthy: true,
+		}
+
+		servers = append(servers, server)
+	}
+
+	return servers
+}
+
+func main() {
+	config := GetConfig()
+	log.Println("Starting load balancer on port: ", config.Port)
+	err := http.ListenAndServe(config.Port, nil)
+	if err != nil {
+		log.Fatalf("Error starting load balancer: %s\n", err.Error())
+	}
+
+	servers := CreateServerList(config.Servers)
+	countOfServers := len(servers)
+	for i := 0; i < countOfServers; i++ {
+		HealthCheck(servers[i], config.HealthCheckInterval)
+	}
 }
