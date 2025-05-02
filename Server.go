eae772f6f0adba44bd23f6e789ff8be71acbd153
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-05-03 03:38:05 +0530

    Added weights in Server

--- Server.go
+++ Server.go
@@ -10,9 +10,11 @@ import (
 )
 
 type Server struct {
-	URL       *url.URL
-	IsHealthy bool
-	Mutex     sync.Mutex
+	URL           *url.URL
+	IsHealthy     bool
+	Weight        int
+	CurrentWeight int
+	Mutex         sync.Mutex
 }
 
 func (s *Server) ReverseProxy() *httputil.ReverseProxy {
