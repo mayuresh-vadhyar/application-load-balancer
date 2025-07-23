096d544631020b8c3a5280c91be886e6e5ba6207
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-07-23 23:06:52 +0530

    Add success writer

---
+++ Success.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ServerResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Id      int    `json:"id"`
+}
+
+func WriteSuccessResponse(w http.ResponseWriter, status int, server *Server) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	response := ServerResponse{
+		Status:  "success",
+		Message: "Server added successfully",
+		Id:      server.Id,
+	}
+	if encodeErr := json.NewEncoder(w).Encode(response); encodeErr != nil {
+		WriteErrorResponse(w, http.StatusInternalServerError, encodeErr.Error())
+	}
+}
