901ccf2ab38e42ec3ac50de8bddb616b64289cb5
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-11-08 21:29:40 +0530

    Implement cooldown routine

--- server/HealthCheck.go
+++ server/HealthCheck.go
@@ -2,13 +2,38 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
-func StartHealthCheckRoutine(ctx context.Context, s *Server, healthCheckInterval time.Duration) {
+func StartHealthCheckRoutine(ctx context.Context, s *Server, healthCheckInterval time.Duration, cooldown time.Duration) {
+	go func ()  {
+		for {
+			runHealthCheck(ctx, s, interval)
+
+			if ctx.Err() != nil {
+				log.Printf("Health check stopped for %s (context canceled)", s.URL)
+				return
+			}
+
+			if cooldown <= 0 {
+				log.Printf("Cooldown not configured. Not restarting health check for %s.", s.URL)
+				return
+			}
+
+			select {
+				case <-ctx.Done():
+					log.Printf("Aborted cooldown restart for %s (context canceled)", s.URL)
+					return
+				case <-time.After(cooldown):
+					log.Printf("Restarting health check for %s after cooldown %v...", s.URL, cooldown)
+			}
+		}
+	}()
+}
+
+func runHealthCheck(ctx context.Context, s *Server, healthCheckInterval time.Duration) {
 	ticker := time.NewTicker(healthCheckInterval)
 	defer ticker.Stop()
 
@@ -19,13 +44,14 @@ func StartHealthCheckRoutine(ctx context.Context, s *Server, healthCheckInterval
 			return
 
 		case <-ticker.C:
+			log.Printf("Check health for %s\n", s.URL.String())
 			res, err := http.Head(s.URL.String())
 			s.Mutex.Lock()
 			if err == nil && res.StatusCode == http.StatusOK {
 				s.IsHealthy = true
 				s.UnhealthyChecks = 0
 			} else {
-				fmt.Printf("%s is down\n", s.URL)
+				log.Printf("%s is down\n", s.URL)
 				if maxUnhealthyChecks > 0 && s.UnhealthyChecks+1 >= maxUnhealthyChecks {
 					DeleteServer(s.URL.String())
 				} else {
