8529071f0b5fcc39895a6d700e2413969872fb35
mayuresh-vadhyar <mvadhyar@gmail.com>
2025-12-06 18:18:44 +0530

    Fix health check restart failure

--- server/HealthCheck.go
+++ server/HealthCheck.go
@@ -12,20 +12,22 @@ func StartHealthCheckRoutine(ctx context.Context, s *Server, maxRestart int8) {
 		var restartCount int8 = 0
 
 		for {
-			runHealthCheck(ctx, s, interval)
+			stoppedByCtx := runHealthCheck(ctx, s, interval)
 
-			if ctx.Err() != nil {
+			if stoppedByCtx {
 				log.Printf("Health check stopped for %s (context canceled)", s.URL)
 				return
 			}
 
 			if cooldown <= 0 {
 				log.Printf("Cooldown not configured. Not restarting health check for %s.", s.URL)
+				DeleteServer(s.URL.String())
 				return
 			}
 
 			if maxRestart > 0 && restartCount >= maxRestart {
 				log.Printf("Max restarts reached for %s. Not restarting health check.", s.URL)
+				DeleteServer(s.URL.String())
 				return
 			}
 
@@ -41,18 +43,17 @@ func StartHealthCheckRoutine(ctx context.Context, s *Server, maxRestart int8) {
 	}()
 }
 
-func runHealthCheck(ctx context.Context, s *Server, healthCheckInterval time.Duration) {
+func runHealthCheck(ctx context.Context, s *Server, healthCheckInterval time.Duration) bool {
 	ticker := time.NewTicker(healthCheckInterval)
 	defer ticker.Stop()
 
 	for {
 		select {
 		case <-ctx.Done():
-			log.Printf("Stopping health check for %s\n", s.URL.String())
-			return
+			// log.Printf("Stopping health check for %s\n", s.URL.String())
+			return true
 
 		case <-ticker.C:
-			log.Printf("Check health for %s\n", s.URL.String())
 			res, err := http.Head(s.URL.String())
 			s.Mutex.Lock()
 			if err == nil && res.StatusCode == http.StatusOK {
@@ -61,11 +62,12 @@ func runHealthCheck(ctx context.Context, s *Server, healthCheckInterval time.Dur
 			} else {
 				log.Printf("%s is down\n", s.URL)
 				if maxUnhealthyChecks > 0 && s.UnhealthyChecks+1 >= maxUnhealthyChecks {
-					DeleteServer(s.URL.String())
-				} else {
-					s.UnhealthyChecks++
-					s.IsHealthy = false
+					s.Mutex.Unlock()
+					s.UnhealthyChecks = 0
+					return false
 				}
+				s.UnhealthyChecks++
+				s.IsHealthy = false
 			}
 			s.Mutex.Unlock()
 		}
