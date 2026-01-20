0a3871c5a054eaffc59c57c1c070c69ac5716ad7
mayuresh-vadhyar <mvadhyar@gmail.com>
2026-01-20 22:58:22 +0530

    Create Redis client file

---
+++ Redis/Redis.go
@@ -0,0 +1,28 @@
+package Redis
+
+import (
+	"context"
+	"sync"
+
+	"github.com/mayuresh-vadhyar/application-load-balancer/config"
+	"github.com/redis/go-redis/v9"
+)
+
+var initOnce sync.Once
+var client *redis.Client
+
+func InitializeRedisClient() *redis.Client {
+	initOnce.Do(func() {
+		config := config.GetConfig()
+		ctx := context.Background()
+		client = redis.NewClient(&redis.Options{
+			Addr: config.RedisURL,
+		})
+
+		_, pingErr := client.Ping(ctx).Result()
+		if pingErr != nil {
+			client = nil
+		}
+	})
+	return client
+}
